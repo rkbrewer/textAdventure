2f6cde8badede3078eb04623497b39b46f4a90ff
Ryan Brewer <webbrewer@gmail.com>
2014-10-15 22:25:26 -0500

    housekeeping

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 textAdventure
 =============
 
-An adventure in coding. Inspired by the 90's BBS hit sensation, LORD.
+An adventure in coding &amp; cold steel. This text adventure is inspired by the 90's BBS hit sensation, LORD (as best I remember it).
+
+Currently this is just a (working) prototype. My goal is to narrate the problem domain through good ol' fashioned expressive code.
+
+Roadmap
+-------
+
+There's not much of a roadmap; I'd like to focus on text animations at some point, and bring my brother in for some creative writing.
+
+It's currently written with [Knockout](http://knockoutjs.com/), and I *do* want to transition to [Angular](https://angularjs.org/) (...[because reasons](http://www.google.com/trends/explore#q=angular%20js%2C%20knockout%20js%2C%20backbone%20js%2C%20ember%20js&cmpt=q)).
+
+Coding Patterns
+---------------
+
+* Addy Osmani's [writeup on Abstract Decorators](http://addyosmani.com/resources/essentialjsdesignpatterns/book/#decoratorpatternjavascript)
+* ES5 Constructor Modules as described in Stoyan Stefanov's [JavaScript Patterns book](http://www.epubbud.com/book.php?g=U4H6WFZJ)
--- js/scenes/battle-scene.js
+++ js/scenes/battle-scene.js
@@ -1,61 +1,39 @@
-/*
- How would an encounter/BattleScene work?
-
- 0. [the BattleScene looks at the turnChart... which states you are first]
- 1. "A slug looms before you. {{ You sense you can easily get the first attack. }}"
- 2. [you push A) Attack]
- 3. "You attack! The slug enrages!" A slash of blood arcs across the screen and fades away. The Slug's HP is decremented.
- 4. [the BattleScene advances down the turnChart... which states the slug is next]
- 5. "The slug fires projectile slime at your face, it burns!" Might and magic style splats animate across the screen, and fade away. Your HP is decremented.
- 6. [the BattleScene advances down the turnChart... which states you are next]
- 7. [you push A) Attack]
- 8. "You attack! The slug is obliterated." You see an arc of blood fade away, and the slug's hp crumples off the page.
- 9.
-
-
-BattleScene is a Scene (by convention).  A SceneManager will instantiate a new BattleScene, and plug it onto the master
-view model (a "scene" node)(cleanNode when changing scenes?)
-
-Other than the naming convention, scenes don't have to have anything in common... other than they are parametized by actors (such as
-player and/or mob).  Let the SceneManager instantiate the Mob (or even delegate instantiation) and pass it into the new BattleScene.
-
+/**
+ * Battle Scene - Decorates Scene (an abstract class)
  */
-
-
-
-// Decorates a Scene, but in the style of new BattleScene(new Scene())
-// http://addyosmani.com/resources/essentialjsdesignpatterns/book/#decoratorpatternjavascript (The Pseudo-Classical Decorator Pattern; Abstract Decorators)
 var BattleScene = (function(){
 
 	function BattleScene( scene ){
 
-		// Invoke the superclass constructor as a method of this object (
 		this.super = scene;
+		this.name = 'scene-battle';	// must match JST name
 
 		this.player = scene.player;
-
-		this.name = 'scene-battle';
-
 		this.mob = new Mob(window.mobData);
+
 		this.turnChart = new TurnChart(this.player, this.mob);
-		this.blog = ko.observable('');
+
+		this.blog = ko.observable('');	// "battle log"
 
 		this.startScene();
 	}
 
 	$.extend(BattleScene.prototype, {
 		startScene:function(){
-			this.super.startScene();	//super
+			this.super.startScene();
 			this.updateBlog("You encounter a " + this.mob.name() + ".");
 		},
+
 		updateBlog:function(message){
 			this.blog( this.blog() + message + '<br>' );
 		},
+
 		attackMob:function(){
 			var attack = this.player.attack(this.mob);
 			this.updateBlog(attack.results);
 			this.endTurn();
 		},
+
 		endTurn:function(){
 			if ( this.turnChart.currentCharacter().isDead() ){ return this.endScene(); }
 
@@ -65,6 +43,7 @@ var BattleScene = (function(){
 				this.mob.takeTurn(this);	// pass a reference to the battle
 			}
 		},
+
 		endScene:function(){
 			var loser = this.turnChart.currentCharacter(),
 				message = loser.isDead() ? loser.name() + " has died." : loser.name() + " has fled.";
@@ -81,11 +60,12 @@ var BattleScene = (function(){
 
 			this.super.endScene();
 		},
+
 		runFromMob:function(){
 			var egress = this.player.run(this.mob);
 
 			if (egress.success){
-				this.endScene(this.player);
+				this.endScene();
 			} else {
 				this.updateBlog(egress.results);
 				this.endTurn();
