6d019bc6c5f1f10af3702c2ead9806de60aa3c8a
Ryan Brewer <webbrewer@gmail.com>
2014-10-17 20:42:19 -0500

    stubbed data for mob/player as JS

--- js/main.js
+++ js/main.js
@@ -2,51 +2,18 @@ $(function(){
 
 	// TODO build a backend to serve up data
 
-	window.playerData = '';
-	window.mobData = '';
-	var requests = [];
+	// TODO move this gunk into a SceneManager somehow
 
-	// For now, hack github pages to get json (http://www.danielx.net/gh-pages-jsonp)
-	requests.push($.ajax({
-		url:'http://webbrewer.github.io/textAdventure/_playerdata.json.js',
-		dataType:'jsonp',
-		jsonpCallback:'webbrewer/textAdventure:_playerdata',
-		success:function(json){
-			window.playerData = json[0];
-		}
-	}));
+	window.viewmodel = {
+		player: new Player(playerData)
+	};
 
-	requests.push($.ajax({
-		url:'http://webbrewer.github.io/textAdventure/_mobdata.json.js',
-		dataType:'jsonp',
-		jsonpCallback:'webbrewer/textAdventure:_mobdata',
-		success:function(json){
-			window.mobData = json[0];
-		}
-	}));
+	viewmodel.scene = ko.observable( new BattleScene(new Scene(viewmodel.player)) );
 
-//	requests.push($.getJSON('_playerdata.json.js', function(json){
-//		window.playerData = json[0];
-//	}));
-//
-//	requests.push($.getJSON('_mobdata.json.js', function(json){
-//		window.mobData = json[0];
-//	}));
+	ko.punches.enableAll();
+	ko.applyBindings(viewmodel);
 
-	$.when.apply($, requests).then(function(){
 
-		// TODO move this gunk into a SceneManager somehow
-
-		window.viewmodel = {
-			player: new Player(playerData)
-		};
-
-		viewmodel.scene = ko.observable( new BattleScene(new Scene(viewmodel.player)) );
-
-		ko.punches.enableAll();
-		ko.applyBindings(viewmodel);
-
-	});
 
 
 	/*
